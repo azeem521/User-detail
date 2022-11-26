4d08a9213a901fafa06c2e3f8ee4b92e2a062512
Mohd <azeemsre1@gmail.com>
2022-11-26 10:37:19 +0530

    filter added

--- main.js
+++ main.js
@@ -23,6 +23,8 @@ var filter=document.getElementById('filter');
 form.addEventListener('submit',addform);
 itemList.addEventListener('click',removeItem);
 
+filter.addEventListener('keyup',filterItem);
+
 function addform(e){
     e.preventDefault();
     var newItem=document.getElementById('item').value;
@@ -46,3 +48,17 @@ function removeItem(e){
     }
 }
 
+
+function filterItem(e){
+    // convert text to lower case
+    var text=e.target.value.toLowerCase();
+    var items=itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+        var itemName=item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) !=-1){
+            item.style.display='block';
+        }else{
+            item.style.display='none';
+        }
+    })
+    }
